6c96667b7bffbc26aecf744006011fcd82394a84
Aimee Tan <aitan@calpoly.edu>
2019-09-26 16:12:50 -0700

    updated point

---
+++ part2/Point.java
@@ -0,0 +1,38 @@
+public class Point
+{
+    private double x;
+    private double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public double getRadius()
+    {
+        double rad = Math.sqrt((x * x) + (y * y));
+        return rad;
+    }
+
+    public double getAngle()
+    {
+        double angle = Math.atan2(x, y);
+        return angle;
+    }
+
+    public Point rotate90()
+    {
+        return new Point (-y, x);
+    }
+} 
