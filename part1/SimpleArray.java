5989f874ef7221e29cc005deda904457eb640c26
Aimee Tan <aitan@calpoly.edu>
2019-09-26 16:01:23 -0700

    updated simplearray

--- part1/SimpleArray.java
+++ part1/SimpleArray.java
@@ -6,7 +6,8 @@ class SimpleArray
          input array.  The length of an array is accessible through
          an array's length field (e.g., values.length).
       */
-      int [] newValues = new int[1];  // This allocates an array of integers.
+
+        int [] newValues = new int[values.length];  // This allocates an array of integers.
 
       /* TO DO: The output array, newValues, should hold as
          its elements the square of the corresponding element
@@ -16,6 +17,13 @@ class SimpleArray
          input array and to place the result into the output array.
       */
 
-      return newValues;
+        for (int i = 0; i < values.length; i++)
+        {
+            int square = values[i] * values[i];   
+            newValues[i] = square;
+        }
+
+        return newValues;
+
    }
 }
