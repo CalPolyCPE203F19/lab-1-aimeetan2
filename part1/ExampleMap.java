971239ec455e5f926b07886d57f931d398c2b6c4
Aimee Tan <aitan@calpoly.edu>
2019-09-26 15:58:47 -0700

    updated examplemap

--- part1/ExampleMap.java
+++ part1/ExampleMap.java
@@ -14,6 +14,21 @@ class ExampleMap
          in a number of units strictly greater than the unitThreshold.
       */
 
+      for (String name : courseListsByStudentName.keySet())
+      {
+         int units = 0;
+
+         for (Course course : courseListsByStudentName.get(name))
+         {
+            units += course.getNumUnits();
+         }
+
+         if (units > unitThreshold)
+         {
+            overEnrolledStudents.add(name);
+         }
+       }
+
       return overEnrolledStudents;      
    }
 }
